d1061838a5e11487323ae8c7b87084c4e806ad51
Dperaltat <peraltadaniel069@gmail.com>
2022-09-01 18:45:07 -0500

    Nueva página con Bulma.io

--- js/Switch.js
+++
@@ -1,6 +0,0 @@
-let themeSwitch = document.querySelector('.themeSwitch');
-let body = document.querySelector('body');
-themeSwitch.onclick = function(){
-  themeSwitch.classList.toggle('active');
-  body.classList.toggle('dark');
-}
